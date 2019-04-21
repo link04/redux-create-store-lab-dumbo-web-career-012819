ec0f7148980f162962c8389087f3419705f8b183
Maximo Bautista <max.bautista97@gmail.com>
2019-04-21 00:14:54 +0000

    Automatically backed up by Learn

--- src/createStore.js
+++ src/createStore.js
@@ -10,6 +10,7 @@ export default function createStore(reducer) {
   const getState = () => {
     return state
   }
+    createStore(reducer).dispatch({ type: '@@INIT' });
 
   return {getState, dispatch}
 }
