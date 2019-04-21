20ea54131fdf2277f9da6b15041a0a17d62ea3fb
Maximo Bautista <max.bautista97@gmail.com>
2019-04-21 00:16:15 +0000

    Automatically backed up by Learn

--- src/createStore.js
+++ src/createStore.js
@@ -2,9 +2,6 @@
 export default function createStore(reducer) {
   let state;
   
-  state === undefined ? 
-  dispatch({ type: '@@INIT' }) : null
-  
   const dispatch = (action) => {
     state = reducer(state, action);
     render();
